4e608352fc84b857104de60a7ca4e8642f047be3
Nileshwar Singh <nileshwarsingh2022@gmail.com>
2026-08-16 12:50:40 +0530

    restructured into backend folder

--- src/models/blacklistSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const blacklistSchema = new mongoose.Schema({
-    token: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now, expires: '7d' }
-});
-
-module.exports = mongoose.model("Blacklist", blacklistSchema);
